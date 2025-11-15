a1651cb6ac830894db59257713535fcf28019ca3
Hiresh Bremanand <hbremanand4@gmail.com>
2025-11-15 17:03:18 +0000

    feat: fast api template

---
+++ api/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.get("/health")
+async def health():
+    return {"status": "healthy"}
+
