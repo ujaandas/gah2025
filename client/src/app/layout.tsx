9d6d0690bd3cf3b49a79d119922b612555a4596c
Hiresh Bremanand <hbremanand4@gmail.com>
2025-11-15 14:19:07 +0000

    initial commit

---
+++ client/src/app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono, IBM_Plex_Sans } from "next/font/google";
+import "./globals.css";
+
+const ibmPlexSans = IBM_Plex_Sans({
+  variable: "--font-ibm-plex-sans",
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+});
+
+export const metadata: Metadata = {
+  title: "Gah2025",
+  description: "Great Agent Hack 2025",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${ibmPlexSans.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ client/src/app/page.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <div
+      className="min-h-screen w-full"
+      style={{
+        backgroundImage: 'radial-gradient(circle, #d1d5db 1px, transparent 1px)',
+        backgroundSize: '20px 20px'
+      }}
+    >
+
+    </div>
+  );
+}
